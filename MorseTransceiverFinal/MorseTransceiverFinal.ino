5609a5e8f6fdabf2892a10aa106ebb4b37bfa86b
NPS-Interns <40642057+NPS-Interns@users.noreply.github.com>
2018-06-29 11:42:05 -0700

    Receiver and transmitter files

---
+++ MorseTransceiverFinal/MorseTransceiverFinal.ino
@@ -0,0 +1,22 @@
+
+/*
+ * Vincent Lopez, Austin Erben
+ * CSIS 10C Final Project
+ * Laser Morse Code Transceiver
+ */
+
+#include <Morse.h> // libary found at: https://github.com/stephenyeargin/arduino-projects.git
+char practice [] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+
+Morse morse(4, 200, 1); // (pinNumber,speed,default)
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  morse.sendmsg(practice); // set your message
+  delay (2000); // delay 2 seconds before repeating of meassage
+}
